c73f823d858de06be021a082947b59d128d4fd3e
PalacioAF <federicopalacio92@gmail.com>
2021-02-26 09:07:15 -0300

    cambio de puerto

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json({extended:true}));
 
 //Puerto
-const PORT=process.env.PORT||4000;
+const port=process.env.port||4000;
 
 //Importar Rutas
 app.use('/api/usuarios',require('./routes/usuarios'));
@@ -24,6 +24,6 @@ app.use('/api/proyectos',require('./routes/proyectos'));
 app.use('/api/tareas',require('./routes/tareas'));
 
 //Arrancar
-app.listen(PORT,()=>{
+app.listen(port,'0.0.0.0',()=>{
     console.log('Bienvenido Palacio');
 });
