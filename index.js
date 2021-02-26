ee87f8b0add6073075cb2f1b91f0326f62ba0bd7
PalacioAF <federicopalacio92@gmail.com>
2021-02-26 09:15:34 -0300

    port

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json({extended:true}));
 
 //Puerto
-const port=process.env.port||4000;
+const port=process.env.PORT ||5000;
 
 //Importar Rutas
 app.use('/api/usuarios',require('./routes/usuarios'));
