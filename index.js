f130baaaf9f0c5f40ef4e949cc7d68acac2e7c0b
PalacioAF <federicopalacio92@gmail.com>
2024-10-29 21:00:51 -0300

    cors

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ conectarDB();
 //habilitar cors
 app.use(cors());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+
 //Habilitar express.json
 app.use(express.json({extended:true}));
 
